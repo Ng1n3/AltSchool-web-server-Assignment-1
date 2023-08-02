8a52461f7532b1da95092e2394a46a11a07c0116
ng1n3 <code.with.muyiwa@gmail.com>
2023-08-02 23:03:57 +0100

    adding 3 question to solution

--- 3-http.js
+++ 3-http.js
@@ -1,18 +1,22 @@
-'use strict'
-const  http = require('http');
+"use strict";
 
-const HOSTNAME = 'localhost';
+// * Using the HTTP module
+// Create an http server
+// Return “Hello world” from the response
+// Ensure when you navigate to the server on the browser, “Hello world” is returned.
+
+const http = require("http");
+
+const HOSTNAME = "localhost";
 const PORT = 3000;
 
-
 function requestService(req, res) {
-    res.writeHead(200);
-    res.end('Hello World');
+	res.writeHead(200);
+	res.end("Hello World");
 }
 
-const server = http.createServer(requestService)
+const server = http.createServer(requestService);
 
 server.listen(PORT, HOSTNAME, () => {
-    console.log(`server is listening at http://${HOSTNAME}:${PORT}`);
-
-})
+	console.log(`server is listening at http://${HOSTNAME}:${PORT}`);
+});
